cec9432135ed4664d386455b0dec7aee1968e6d8
effective-range <info@effective-range.com>
2025-04-11 19:50:43 +0000

    Bump version to 0.1.3 for MrHAT platform driver

--- src/er-mrhat-plat.c
+++ src/er-mrhat-plat.c
@@ -184,4 +184,4 @@ MODULE_AUTHOR("Ferenc Janky");
 MODULE_AUTHOR("Attila Gombos");
 MODULE_DESCRIPTION(
     "Platform driver for the MrHATv1 family of RaspberryPi extension hats");
-MODULE_VERSION("0.1.1");
+MODULE_VERSION("0.1.3");
